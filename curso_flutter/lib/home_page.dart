21ccd71d66aa8a334eded6293f9b718f41722ab0
Gustavo Brendon <guhgomesyt@gmail.com>
2024-02-28 17:53:53 -0300

    Add Files

---
+++ curso_flutter/lib/home_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+  @override
+  State<HomePage> createState() {
+    return HomePageState();
+  }
+}
+
+class HomePageState extends State<HomePage> {
+  int counter = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: const Text(
+          'CelPhone',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: Center(
+          child: GestureDetector(
+        child: Text(
+          'Contador: $counter',
+          style: const TextStyle(fontSize: 20),
+        ),
+        onTap: () {
+          setState(() {
+            counter++;
+          });
+        },
+      )),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.red,
+        child: const Icon(Icons.add),
+        onPressed: () {
+          setState(() {
+            counter++;
+          });
+        },
+      ),
+    );
+  }
+}
